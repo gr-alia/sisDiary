95fd8c0e62ddc5a41af358b1fe3ff008d4501df0
Alyona Kalytenko <helena.ytd@gmail.com>
2017-01-14 12:12:36 +0200

    Create DB in DatabaseHelper

---
+++ app/src/main/java/com/alia/sisdiary/SisdiaryDatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.alia.sisdiary;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Alyona on 14.01.2017.
+ */
+
+public class SisdiaryDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "sisdiary";
+    private static final int DB_VERSION = 1;
+
+    SisdiaryDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE SUBJECT (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DAY TEXT, "
+                + "HOMEWORK TEXT);");
+        insertSubject(db, "Математика", "ПН", "домашка");
+        insertSubject(db, "Фіз-ра", "ПН","домашка");
+        insertSubject(db, "Укр. мова", "ПН","домашка");
+        insertSubject(db, "Біологія", "ПН", "домашка");
+        insertSubject(db, "Біологія", "ПН", "домашка");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private static void insertSubject(SQLiteDatabase db, String name, String day, String homework){
+        ContentValues subjectValues = new ContentValues();
+        subjectValues.put("NAME", name);
+        subjectValues.put("DAY", day);
+        subjectValues.put("HOMEWORK", homework);
+        db.insert("SUBJECT", null, subjectValues);
+    }
+}
