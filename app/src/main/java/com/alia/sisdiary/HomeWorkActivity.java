4135698a6c3db38ea4978c03cc108312a7c0a7ff
Alyona Kalytenko <helena.ytd@gmail.com>
2017-01-16 17:18:55 +0200

    Make the work with DB in  background thread using AsyncTask

--- app/src/main/java/com/alia/sisdiary/HomeWorkActivity.java
+++ app/src/main/java/com/alia/sisdiary/HomeWorkActivity.java
@@ -5,7 +5,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.StrictMode;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -57,46 +59,67 @@ public class HomeWorkActivity extends AppCompatActivity {
 
     public void onAddHomeWork(View view) {
         EditText homeWorkEdit = (EditText) findViewById(R.id.editHomeWork);
-        ContentValues homeworkValues = new ContentValues();
-        homeworkValues.put("HOMEWORK", homeWorkEdit.getText().toString());
-        updateHomeWork(homeworkValues);
+        int subjectNO = (Integer) getIntent().getExtras().get(EXTRA_SUBJECTNO);
+        new UpdateHomeWorkTask(homeWorkEdit.getText().toString()).execute(subjectNO);
     }
 
     public void onDeleteHomeWork(View view) {
-        ContentValues homeworkValues = new ContentValues();
-        homeworkValues.put("HOMEWORK", "   ");
-        updateHomeWork(homeworkValues);
-    }
-    private void updateHomeWork(ContentValues homeworkValues){
         int subjectNO = (Integer) getIntent().getExtras().get(EXTRA_SUBJECTNO);
-        SQLiteOpenHelper sisdiaryDatabaseHelper = new SisdiaryDatabaseHelper(this);
-        try {
-            //clear current homework in DB here
-            SQLiteDatabase db = sisdiaryDatabaseHelper.getWritableDatabase();
-            db.update("SUBJECT",
-                    homeworkValues,
-                    "_id=?",
-                    new String[]{Integer.toString(subjectNO)});
-            //read data from DB here
-            Cursor cursor = db.query("SUBJECT",
-                    new String[]{"HOMEWORK"},
-                    "_id = ?",
-                    new String[]{Integer.toString(subjectNO)},
-                    null, null, null);
-            //move to row
-            if (cursor.moveToFirst()) {
-                //get values of row from Cursor
-                String homeworkText = cursor.getString(0);
+        new UpdateHomeWorkTask(" ").execute(subjectNO);
+    }
 
+    private class UpdateHomeWorkTask extends AsyncTask<Integer, Void, Boolean> {
+        ContentValues homeworkValues;
+        String homeworkText;
+        String value;
+        public UpdateHomeWorkTask(String value){
+            this.value = value;
+        }
+        protected void onPreExecute(){
+            homeworkValues = new ContentValues();
+            homeworkValues.put("HOMEWORK", value);
+        }
+        protected Boolean doInBackground(Integer... subjects) {
+            int subjectNO = subjects[0];
+            SQLiteOpenHelper sisdiaryDatabaseHelper = new SisdiaryDatabaseHelper(HomeWorkActivity.this);
+            try {
+                //clear current homework in DB here
+                SQLiteDatabase db = sisdiaryDatabaseHelper.getWritableDatabase();
+                db.update("SUBJECT",
+                        homeworkValues,
+                        "_id=?",
+                        new String[]{Integer.toString(subjectNO)});
+                //read data from DB here
+                Cursor cursor = db.query("SUBJECT",
+                        new String[]{"HOMEWORK"},
+                        "_id = ?",
+                        new String[]{Integer.toString(subjectNO)},
+                        null, null, null);
+                //move to row
+                if (cursor.moveToFirst()) {
+                    //get values of row from Cursor
+                    homeworkText = cursor.getString(0);
+
+                }
+                cursor.close();
+                db.close();
+                return true;
+            } catch (SQLiteException e) {
+               return false;
+            }
+        }
+        protected void onPostExecute(Boolean success){
+            if (!success) {
+                Toast toast = Toast.makeText(HomeWorkActivity.this,
+                        "Database unavailable", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else {
                 //fill in subject homework
                 TextView homework = (TextView) findViewById(R.id.homeWork);
                 homework.setText(homeworkText);
             }
-            cursor.close();
-            db.close();
-        } catch (SQLiteException e) {
-            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
-            toast.show();
         }
+
     }
 }
