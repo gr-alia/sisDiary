d231ac6802058209d4ba8625cd3b193a3a02c8a9
Alyona Kalytenko <helena.ytd@gmail.com>
2017-01-17 10:28:36 +0200

    All actions with DB in background thread

--- app/src/main/java/com/alia/sisdiary/TimetableActivity.java
+++ app/src/main/java/com/alia/sisdiary/TimetableActivity.java
@@ -6,10 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -22,27 +21,7 @@ public class TimetableActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ListView listView = getListView();
-        try {
-            SQLiteOpenHelper sisdiaryDatabaseHelper = new SisdiaryDatabaseHelper(this);
-            db = sisdiaryDatabaseHelper.getReadableDatabase();
-            cursor = db.query("SUBJECT",
-                    new String[]{"_id","NAME"},
-                    null, null, null, null, null
-            );
-            CursorAdapter cursorAdapter = new SimpleCursorAdapter(
-                    this,
-                    android.R.layout.simple_list_item_1,
-                    cursor,
-                    new String[]{"NAME"},
-                    new int[]{android.R.id.text1},
-                    0);
-            listView.setAdapter(cursorAdapter);
-        } catch (SQLiteException e) {
-            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_LONG);
-            toast.show();
-        }
-
+        new CreateTimetableTask().execute();
     }
     @Override
     public  void onDestroy(){
@@ -59,5 +38,40 @@ public class TimetableActivity extends ListActivity {
         Intent intent = new Intent(this, HomeWorkActivity.class);
         intent.putExtra(HomeWorkActivity.EXTRA_SUBJECTNO, (int) id);
         startActivity(intent);
+    }
+    private class CreateTimetableTask extends AsyncTask<Void, Void, Boolean>{
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                SQLiteOpenHelper sisdiaryDatabaseHelper = new SisdiaryDatabaseHelper(TimetableActivity.this);
+                db = sisdiaryDatabaseHelper.getReadableDatabase();
+                cursor = db.query("SUBJECT",
+                        new String[]{"_id","NAME"},
+                        null, null, null, null, null
+                );
+                return true;
+            } catch (SQLiteException e) {
+                return false;
+            }
+        }
+        protected void onPostExecute(Boolean success){
+            if (!success) {
+                Toast toast = Toast.makeText(TimetableActivity.this,
+                        "Database unavailable", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else {
+                ListView listView = getListView();
+                CursorAdapter cursorAdapter = new SimpleCursorAdapter(
+                        TimetableActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        cursor,
+                        new String[]{"NAME"},
+                        new int[]{android.R.id.text1},
+                        0);
+                listView.setAdapter(cursorAdapter);
+            }
+        }
     }
 }
